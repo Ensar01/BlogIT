6bc07054b2da99e28bce2b8788894228282e4335
Ensar Mulahmetović <ensar.mulahmetovic@edu.fit.ba>
2025-02-15 20:40:54 +0100

    INIT

--- Backend/BlogIT/BlogIT/Program.cs
+++ Backend/BlogIT/BlogIT/Program.cs
@@ -10,17 +10,15 @@ namespace BlogIT
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                app.MapOpenApi();
             }
 
             app.UseCors(
@@ -29,7 +27,7 @@ namespace BlogIT
                      .AllowAnyMethod()
                      .AllowAnyHeader()
                      .AllowCredentials()
-); //This needs to set everything allowed
+        ); //This needs to set everything allowed
 
             app.UseHttpsRedirection();
 
@@ -39,7 +37,6 @@ namespace BlogIT
             app.MapControllers();
 
             app.Run();
-
         }
     }
 }
