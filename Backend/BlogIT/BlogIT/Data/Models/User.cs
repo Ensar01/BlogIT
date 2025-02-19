988ccb9d1943406c5a436e9e91bf7e2d5b415fdf
Ensar Mulahmetović <ensar.mulahmetovic@edu.fit.ba>
2025-02-19 22:32:36 +0100

    UserAdded

---
+++ Backend/BlogIT/BlogIT/Data/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace BlogIT.Data.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateOnly BirthDate { get; set; }
+        public DateOnly RegistrationDate { get; set; }
+
+    }
+}
