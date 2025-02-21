5ea4915cc8dab1ef3d57b40875950eaa9358d491
Ensar Mulahmetović <ensar.mulahmetovic@edu.fit.ba>
2025-02-21 23:00:45 +0100

    UserController

---
+++ Backend/BlogIT/BlogIT/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using BlogIT.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogIT.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
+    }
+}
