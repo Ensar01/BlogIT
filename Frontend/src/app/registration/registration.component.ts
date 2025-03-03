0b4a76f0bb32f801c84680a870af6de4f7a88ff6
Ensar Mulahmetović <ensar.mulahmetovic@edu.fit.ba>
2025-03-04 00:47:49 +0100

    C2 - Frontend register page edit

--- Frontend/src/app/registration/registration.component.ts
+++ Frontend/src/app/registration/registration.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit, OnDestroy} from '@angular/core';
 import {NgOptimizedImage} from '@angular/common';
-import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {RouterLink} from '@angular/router';
 
 
 @Component({
   selector: 'app-registration',
   imports: [
     NgOptimizedImage,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterLink
   ],
   templateUrl: './registration.component.html',
   standalone: true,
@@ -15,7 +17,13 @@ import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 })
 
 export class RegistrationComponent {
-  
+  registerForm = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    username: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
+  })
 
 
 
