c2fe2375ce6505be89dee7bf179e05476c0fe0d4
Ensar Mulahmetović <ensar.mulahmetovic@edu.fit.ba>
2025-03-08 00:26:18 +0100

    C8 - Landing page component added

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import {RegistrationComponent} from './registration/registration.component';
+import {LandingPageComponent} from './landing-page/landing-page.component';
 
 export const routes: Routes = [
-  {path: '', redirectTo: 'register', pathMatch: 'full'},
+  {path: '', redirectTo: 'landing-page', pathMatch: 'full'},
+  {path: 'landing-page', component: LandingPageComponent},
   {path: 'register',component: RegistrationComponent}
+
 ];
